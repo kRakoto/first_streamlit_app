a78b157c59985eb0e660eb3aba5e9fe8a9e80550
kRakoto <70438244+kRakoto@users.noreply.github.com>
2023-01-09 11:18:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import streamlit
 ## adding title to my app
 streamlit.title('Trainning for Mell s parent new diner')
 
-strelit.title('this would be second title')
+print('this would be second title')
