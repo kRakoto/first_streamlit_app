bf7883989cb04a58c0049e3d4a10e32e1daadf0d
kRakoto <70438244+kRakoto@users.noreply.github.com>
2023-01-09 11:29:37 -0500

    Update streamlit_app.py
    open a csv and load it in a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 
 import streamlit
+import pandas
 
 ## adding title to my app
 streamlit.title('Trainning for Mell s parent new diner')
@@ -7,3 +8,9 @@ streamlit.title('Trainning for Mell s parent new diner')
 streamlit.header('Breakfast Menu')
 
 streamlit.text('🐔 hard-Boiled Free-Range egg')
+
+##importing with pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+##open thfile and display it in the app
+streamlit.dataframe(my_fruit_list)
