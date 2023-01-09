1d51d6c78198afb95fb11128eb0652c2e08b866a
kRakoto <70438244+kRakoto@users.noreply.github.com>
2023-01-09 18:03:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,13 @@ streamlit.title('Trainning for Mell s parent new diner')
 streamlit.header('Breakfast Menu')
 
 streamlit.text('🐔 hard-Boiled Free-Range egg')
+
+
+
+
+##importing with pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 ##open thfile and display it in the app
 streamlit.dataframe(my_fruit_list)
 
@@ -17,12 +24,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 ##add multiselect based on Fruit
 fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-
-
-##importing with pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
 
 
 ##Display it in our app
