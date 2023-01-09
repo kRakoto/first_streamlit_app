832e720568a6f70c74650ba1a10463941407f477
kRakoto <70438244+kRakoto@users.noreply.github.com>
2023-01-09 12:00:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,15 @@ streamlit.text('🐔 hard-Boiled Free-Range egg')
 ##importing with pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+##set index to the fruits in my fruit list
+##in this way we will get the Fruit label as index
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 ##open thfile and display it in the app
 streamlit.dataframe(my_fruit_list)
 
 ##add multiselect based on Fruit
-fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.Fruit))
+fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 ##Display it in our app
